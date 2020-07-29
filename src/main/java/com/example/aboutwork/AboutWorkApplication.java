518d30452f4691ad1db8d988902824d39bbfae31
杨葱 <cong.yang@baozun.com>
2020-07-29 15:37:10 +0800

    初始化项目

---
+++ src/main/java/com/example/aboutwork/AboutWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.aboutwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AboutWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aboutwork/AboutWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aboutwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
