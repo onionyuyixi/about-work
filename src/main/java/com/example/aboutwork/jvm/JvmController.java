65be892d04eb1389668a624d75c11f69cd79b81e
杨葱 <cong.yang@baozun.com>
2020-09-18 16:29:26 +0800

    add jvm part

---
+++ src/main/java/com/example/aboutwork/jvm/JvmController.java
@@ -0,0 +1,28 @@
+package com.example.aboutwork.jvm;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@RestController
+@RequestMapping("/jvm/")
+public class JvmController {
+
+
+            ConcurrentHashMap<Object, Object> map = new ConcurrentHashMap<>();
+    @GetMapping("doLoop")
+    public void doLoop() {
+        while (true) {
+            map.put(new Object(),new Object());
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int x = Runtime.getRuntime().availableProcessors();
+        System.err.println(x);
+    }
+
+}
