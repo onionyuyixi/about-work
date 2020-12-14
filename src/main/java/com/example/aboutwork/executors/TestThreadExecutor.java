3930e4581a2d99a814455819735c6e3b127e4ad9
杨葱 <cong.yang@baozun.com>
2020-12-14 09:49:03 +0800

    add  explain

--- src/main/java/com/example/aboutwork/executors/TestThreadExecutor.java
+++ src/main/java/com/example/aboutwork/executors/TestThreadExecutor.java
@@ -11,7 +11,7 @@ public class TestThreadExecutor {
     private static final int COUNT_BITS = Integer.SIZE - 3;
     private static final int CAPACITY = (1 << COUNT_BITS) - 1;
 
-    // runState is stored in the high-order bits
+    // runState is stored in the high-order bits  運行狀態保存在最高位上
     private static final int RUNNING = -1 << COUNT_BITS;
     private static final int SHUTDOWN = 0 << COUNT_BITS;
     private static final int STOP = 1 << COUNT_BITS;
