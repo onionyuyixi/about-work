d6bb5464c398aa086592a6cbf7c5cbff12da354e
杨葱 <cong.yang@baozun.com>
2020-12-25 20:20:31 +0800

    test thread pool

--- src/main/java/com/example/aboutwork/executors/TestThreadExecutor.java
+++ src/main/java/com/example/aboutwork/executors/TestThreadExecutor.java
@@ -3,7 +3,11 @@ package com.example.aboutwork.executors;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 public class TestThreadExecutor {
 
@@ -22,6 +26,7 @@ public class TestThreadExecutor {
 
     /**
      * 结果只有~CAPACITY 和 0 两种
+     *
      * @param c
      * @return
      */
@@ -31,6 +36,7 @@ public class TestThreadExecutor {
 
     /**
      * 永远是个非负数
+     *
      * @param c
      * @return
      */
@@ -41,8 +47,9 @@ public class TestThreadExecutor {
 
     /**
      * 结果是 rs + wc 之和
-     * @param rs  ~CAPACITY / 0
-     * @param wc  非负数
+     *
+     * @param rs ~CAPACITY / 0
+     * @param wc 非负数
      * @return
      */
     private static int ctlOf(int rs, int wc) {
@@ -50,33 +57,184 @@ public class TestThreadExecutor {
     }
 
     @Test
-    public void testRunStateOf(){
+    public void testRunStateOf() {
         int a = 100;
         int b = -100;
-        Assert.assertEquals(runStateOf(a),0);
-        Assert.assertEquals(runStateOf(b),~CAPACITY);
+        Assert.assertEquals(runStateOf(a), 0);
+        Assert.assertEquals(runStateOf(b), ~CAPACITY);
     }
 
     @Test
-    public void testWorkerCountOf(){
+    public void testWorkerCountOf() {
         int a = 100;
         int b = -100;
-        Assert.assertEquals(workerCountOf(a),a);
-        Assert.assertEquals(workerCountOf(b)-1,CAPACITY-100);
+        Assert.assertEquals(workerCountOf(a), a);
+        Assert.assertEquals(workerCountOf(b) - 1, CAPACITY - 100);
     }
 
     @Test
-    public void testCltOf(){
+    public void testCltOf() {
         int b = 10;
-        Assert.assertEquals(ctlOf(0,b),rsPlusWc(0,b));
+        Assert.assertEquals(ctlOf(0, b), rsPlusWc(0, b));
         int f = 0;
-        Assert.assertEquals(ctlOf(~CAPACITY,f),rsPlusWc(~CAPACITY,f));
+        Assert.assertEquals(ctlOf(~CAPACITY, f), rsPlusWc(~CAPACITY, f));
         int g = 100;
-        Assert.assertEquals(ctlOf(~CAPACITY,g),rsPlusWc(~CAPACITY,g));
+        Assert.assertEquals(ctlOf(~CAPACITY, g), rsPlusWc(~CAPACITY, g));
     }
 
-    private int rsPlusWc(int rs , int wc){
-        return rs+wc;
+    private int rsPlusWc(int rs, int wc) {
+        return rs + wc;
     }
 
+
+    private final class Worker
+            extends AbstractQueuedSynchronizer
+            implements Runnable {
+        /**
+         * This class will never be serialized, but we provide a
+         * serialVersionUID to suppress a javac warning.
+         */
+        private static final long serialVersionUID = 6138294804551838833L;
+
+        /**
+         * Thread this worker is running in.  Null if factory fails.
+         */
+        final Thread thread;
+        /**
+         * Initial task to run.  Possibly null.
+         */
+        Runnable firstTask;
+        /**
+         * Per-thread task counter
+         */
+        volatile long completedTasks;
+
+        /**
+         * Creates with given first task and thread from ThreadFactory.
+         *
+         * @param firstTask the first task (null if none)
+         */
+        Worker(Runnable firstTask) {
+            setState(-1); // inhibit interrupts until runWorker
+            this.firstTask = firstTask;
+            this.thread = new Thread(this);
+        }
+
+        /**
+         * Delegates main run loop to outer runWorker
+         */
+        public void run() {
+            runWorker(this);
+        }
+
+        // Lock methods
+        //
+        // The value 0 represents the unlocked state.
+        // The value 1 represents the locked state.
+
+        protected boolean isHeldExclusively() {
+            return getState() != 0;
+        }
+
+        protected boolean tryAcquire(int unused) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        protected boolean tryRelease(int unused) {
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        public void lock() {
+            acquire(1);
+        }
+
+        public boolean tryLock() {
+            return tryAcquire(1);
+        }
+
+        public void unlock() {
+            release(1);
+        }
+
+        public boolean isLocked() {
+            return isHeldExclusively();
+        }
+
+        void interruptIfStarted() {
+            Thread t;
+            if (getState() >= 0 && (t = thread) != null && !t.isInterrupted()) {
+                try {
+                    t.interrupt();
+                } catch (SecurityException ignore) {
+                }
+            }
+        }
+    }
+
+
+    final void runWorker(Worker w) {
+        Thread wt = Thread.currentThread();
+        Runnable task = w.firstTask;
+        w.firstTask = null;
+        w.unlock(); // allow interrupts
+        boolean completedAbruptly = true;
+        try {
+            while (task != null) {
+                w.lock();
+                // If pool is stopping, ensure thread is interrupted;
+                // if not, ensure thread is not interrupted.  This
+                // requires a recheck in second case to deal with
+                // shutdownNow race while clearing interrupt
+                if ((ctl.get() < STOP) ||
+                        (Thread.interrupted() &&
+                                (ctl.get() > STOP)) &&
+                                !wt.isInterrupted())
+                    wt.interrupt();
+                try {
+                    Throwable thrown = null;
+                    try {
+                        task.run();
+                    } catch (RuntimeException x) {
+                        thrown = x;
+                        throw x;
+                    } catch (Error x) {
+                        thrown = x;
+                        throw x;
+                    } catch (Throwable x) {
+                        thrown = x;
+                        throw new Error(x);
+                    }
+                } finally {
+                    task = null;
+                    w.completedTasks++;
+                    w.unlock();
+                }
+            }
+            completedAbruptly = false;
+        }catch (Exception e){
+            System.err.println(e.getStackTrace());
+        }
+    }
+
+
+    @Test
+    public  void testRunWorker(){
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        for (int i = 0; i < 100; i++) {
+            int finalI = i;
+            if(finalI/4==0) {
+                Worker worker = new Worker(() -> System.err.println("yuyixi" + finalI));
+                executorService.execute(worker);
+            }else {
+                executorService.execute(new Worker(null));
+            }
+        }
+
+    }
 }
